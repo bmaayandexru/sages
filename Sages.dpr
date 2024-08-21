d21684ba50a17ce25753878bb79b298308e08db7
Михаил А. Борисенко <BMA@start-npf.yaroslavl.local>
2024-08-21 12:01:18 +0300

    first commit

---
+++ Sages.dpr
@@ -0,0 +1,14 @@
+program Sages;
+
+uses
+  Vcl.Forms,
+  UfrmMain in 'UfrmMain.pas' {frmMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmMain, frmMain);
+  Application.Run;
+end.
